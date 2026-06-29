28a1d04c979594adfba662bfd1120b7887375966
dukicitav <dukicitav@gmail.com>
2026-06-29 10:14:00 +0200

    Initial commit from Create Next App

---
+++ next.config.ts
@@ -0,0 +1,19 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  reactCompiler: true,
+
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
+    ],
+  },
+  output: 'standalone',
+};
+
+export default nextConfig;
